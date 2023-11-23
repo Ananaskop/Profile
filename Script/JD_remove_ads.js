641424ce653c72a4421ee484c03610cdfe93ced9
Gene <123@gmail.com>
2023-11-23 02:48:38 +0000

    Update JS

--- Script/JD_remove_ads.js
+++ Script/JD_remove_ads.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/myBlockAds.js
-// 更新时间：2023-11-23 04:08:00
+// 更新时间：2023-11-23 10:48:38
 // 2023-11-14 07:25
 
 const url = $request.url;

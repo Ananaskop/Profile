4b4fbf2986e24ec74ee8104563b74397d946b6c3
Gene Wong <gene@likesto.me>
2023-11-29 21:17:01 +0800

    更新 net-info-panel.js

--- Script/net-info-panel.js
+++ Script/net-info-panel.js
@@ -197,7 +197,7 @@ function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
                 `[IP地址]\n` +
                 getIP() +
                 `[节点IP] ${info.query}\n` +
-                `[节点ISP] ${info.isp}\n` +
+                `[节点服务商] ${info.isp}\n` +
                 `[节点位置] ${info.country} - ${info.city}`,
             icon: getSSID() ? 'wifi' : 'simcard',
             'icon-color': getSSID() ? '#005CAF' : '#F9BF45',

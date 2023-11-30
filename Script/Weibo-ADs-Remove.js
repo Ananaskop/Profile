2353b3c72a40a81acca8414d362c1a688d795499
Gene <123@gmail.com>
2023-11-30 12:18:16 +0000

    Update JS

--- Script/Weibo-ADs-Remove.js
+++ Script/Weibo-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/weibo.js
-// 更新时间：2023-11-30 14:13:02
+// 更新时间：2023-11-30 20:18:15
 // 2023-11-26 16:35
 
 const url = $request.url;

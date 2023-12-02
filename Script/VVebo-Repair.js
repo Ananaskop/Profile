5aa1e85c012606157cfedcf9336759f0c0fc5336
Gene <123@gmail.com>
2023-12-02 16:01:17 +0800

    更新文件

--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -7,21 +7,16 @@ function getUid(url) {
     return undefined;
 }
 
-const isQuantumultX = typeof $prefs !== "undefined";
+let uid; 
 
 if (url.includes("users/show")) {
-    const uid = getUid(url);
+    uid = getUid(url);
     if (uid) {
-        if (isQuantumultX) {
-            $prefs.setValueForKey(uid, "uid");
-        } else {
-            $persistentStore.write(uid, "uid");
-        }
+        $done({});
     }
-    $done({});
 } else if (url.includes("statuses/user_timeline")) {
-    const storedUid = isQuantumultX ? $prefs.valueForKey("uid") : $persistentStore.read("uid");
-    const uid = getUid(url) || storedUid;
+    const storedUid = uid || $persistentStore.read("uid");
+    uid = getUid(url) || storedUid;
     const newUrl = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id") + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
     $done({ url: newUrl });
 } else if (url.includes("profile/statuses/tab")) {

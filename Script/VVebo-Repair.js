7d49994e8c8f823ce2f5592a6fc71bb74d1a2cce
Gene <123@gmail.com>
2023-12-02 15:49:21 +0800

    Refactor VVebo-Repair.js file

--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -1,25 +1,33 @@
-// 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2023-12-02 14:12:04
-let url = $request.url;
-let hasUid = (url) => url.includes("uid");
-let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
+const url = $request.url;
+
+function getUid(url) {
+    if (url.includes("uid")) {
+        return url.match(/uid=(\d+)/)[1];
+    }
+    return undefined;
+}
+
 if (url.includes("users/show")) {
-  $persistentStore.write(getUid(url), "uid");
-  $done({});
+    const uid = getUid(url);
+    if (uid) {
+        $persistentStore.write(uid, "uid"); // For Surge
+        $prefs.setValueForKey(uid, "uid"); // For Quantumult X
+    }
+    $done({});
 } else if (url.includes("statuses/user_timeline")) {
-  let uid = getUid(url) || $persistentStore.read("uid");
-  url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
-  url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
-  $done({ url });
+    const storedUid = $persistentStore.read("uid") || $prefs.valueForKey("uid");
+    const uid = getUid(url) || storedUid;
+    const newUrl = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id") + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
+    $done({ url: newUrl });
 } else if (url.includes("profile/statuses/tab")) {
-  let data = JSON.parse($response.body);
-  let statuses = data.cards
-    .map((card) => (card.card_group ? card.card_group : card))
-    .flat()
-    .filter((card) => card.card_type === 9)
-    .map((card) => card.mblog);
-  let sinceId = data.cardlistInfo.since_id;
-  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+    const data = JSON.parse($response.body);
+    const statuses = data.cards
+        .map(card => (card.card_group ? card.card_group : card))
+        .flat()
+        .filter(card => card.card_type === 9)
+        .map(card => card.mblog);
+    const sinceId = data.cardlistInfo.since_id;
+    $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
 } else {
-  $done({});
+    $done({});
 }

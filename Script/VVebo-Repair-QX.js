e8738e13f23c5711f155757b89e53c92ab583600
Gene <123@gmail.com>
2023-12-04 11:35:27 +0800

    更新文件

--- Script/VVebo-Repair-QX.js
+++ Script/VVebo-Repair-QX.js
@@ -13,23 +13,36 @@ if (url.includes("users/show")) {
   }
   $done({});
 } else if (url.includes("statuses/user_timeline")) {
-  if (typeof $persistentStore !== 'undefined') {
-    uid = getUid(url) || $persistentStore.read("uid");
-  } else {
-    uid = getUid(url) || $prefs.valueForKey("uid");
-  }
-  url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
-  url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
-  $done({ url });
+    try {
+        let data = JSON.parse($response.body);
+        let statuses = data.cards
+            .map((card) => (card.card_group ? card.card_group : card))
+            .flat()
+            .filter((card) => card.card_type === 9)
+            .map((card) => card.mblog);
+        let sinceId = data.cardlistInfo.since_id;
+        $done({
+            body: JSON.stringify({
+                statuses,
+                since_id: sinceId,
+                total_number: 100
+            })
+        });
+    } catch (error) {
+        if (typeof $persistentStore !== 'undefined') {
+            uid = getUid(url) || $persistentStore.read("uid");
+        } else {
+            uid = getUid(url) || $prefs.valueForKey("uid");
+        }
+        url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
+        url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
+        $done({
+            url
+        });
+    }
 } else if (url.includes("profile/statuses/tab")) {
-  let data = JSON.parse($response.body);
-  let statuses = data.cards
-    .map((card) => (card.card_group ? card.card_group : card))
-    .flat()
-    .filter((card) => card.card_type === 9)
-    .map((card) => card.mblog);
-  let sinceId = data.cardlistInfo.since_id;
-  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+    console.log('ss');
 } else {
-  $done({});
+    $done({});
 }
+

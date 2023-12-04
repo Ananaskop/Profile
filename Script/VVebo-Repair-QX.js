9c193857e188444906d0a55b4842e1ec6799b0d8
Gene <123@gmail.com>
2023-12-04 14:08:13 +0800

    Fix issues with getting and setting uid in VVebo-Repair-QX.js and VVebo-Repair.js

--- Script/VVebo-Repair-QX.js
+++ Script/VVebo-Repair-QX.js
@@ -16,38 +16,47 @@ hostname = api.weibo.cn
 *****************************************/
 
 let url = $request.url;
+
+// Function to check if a key exists in either $prefs or $persistentStore
+const getValueForKey = (key) => {
+    if ($prefs) {
+        return $prefs.valueForKey(key);
+    } else if ($persistentStore) {
+        return $persistentStore.read(key);
+    } else {
+        return undefined; // Both $prefs and $persistentStore are not available
+    }
+};
+
+// Function to set a key-value pair in either $prefs or $persistentStore
+const setValueForKey = (key, value) => {
+    if ($prefs) {
+        $prefs.setValueForKey(value, key);
+    } else if ($persistentStore) {
+        $persistentStore.write(value, key);
+    }
+    // Ignore if both $prefs and $persistentStore are not available
+};
+
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
+
 if (url.includes("users/show")) {
-    $prefs.setValueForKey(getUid(url), "weibouid");
+    setValueForKey("uid", getUid(url));
     $done({});
 } else if (url.includes("statuses/user_timeline")) {
-    try{
-        let data = JSON.parse($response.body);
-        let statuses = data.cards
-            .map((card) => (card.card_group ? card.card_group : card))
-            .flat()
-            .filter((card) => card.card_type === 9)
-            .map((card) => card.mblog);
-        let sinceId = data.cardlistInfo.since_id;
-        $done({
-            body: JSON.stringify({
-                statuses,
-                since_id: sinceId,
-                total_number: 100
-            })
-        });
-    } catch {
-        let uid = getUid(url) || $prefs.valueForKey("weibouid");
+    let uid = getUid(url) || getValueForKey("uid");
+    if (uid) {
         url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
         url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
-        $done({
-            url
-        });
+        $done({ url });
+    } else {
+        // Handle the case when uid is not available
+        $done({});
     }
-
 } else if (url.includes("profile/statuses/tab")) {
     console.log('ss');
 } else {
     $done({});
 }
+
--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -1,25 +1,46 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
 // 更新时间：2023-12-04 12:03:41
 let url = $request.url;
+
+// Function to check if a key exists in either $prefs or $persistentStore
+const getValueForKey = (key) => {
+    if ($prefs) {
+        return $prefs.valueForKey(key);
+    } else if ($persistentStore) {
+        return $persistentStore.read(key);
+    } else {
+        return undefined; // Both $prefs and $persistentStore are not available
+    }
+};
+
+// Function to set a key-value pair in either $prefs or $persistentStore
+const setValueForKey = (key, value) => {
+    if ($prefs) {
+        $prefs.setValueForKey(value, key);
+    } else if ($persistentStore) {
+        $persistentStore.write(value, key);
+    }
+    // Ignore if both $prefs and $persistentStore are not available
+};
+
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
+
 if (url.includes("users/show")) {
-  $persistentStore.write(getUid(url), "uid");
-  $done({});
+    setValueForKey("uid", getUid(url));
+    $done({});
 } else if (url.includes("statuses/user_timeline")) {
-  let uid = getUid(url) || $persistentStore.read("uid");
-  url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
-  url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
-  $done({ url });
+    let uid = getUid(url) || getValueForKey("uid");
+    if (uid) {
+        url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
+        url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
+        $done({ url });
+    } else {
+        // Handle the case when uid is not available
+        $done({});
+    }
 } else if (url.includes("profile/statuses/tab")) {
-  let data = JSON.parse($response.body);
-  let statuses = data.cards
-    .map((card) => (card.card_group ? card.card_group : card))
-    .flat()
-    .filter((card) => card.card_type === 9)
-    .map((card) => card.mblog);
-  let sinceId = data.cardlistInfo.since_id;
-  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
+    console.log('ss');
 } else {
-  $done({});
+    $done({});
 }

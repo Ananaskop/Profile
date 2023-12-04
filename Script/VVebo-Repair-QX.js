18eda5b2b18d7ee86205a2968b6324ff1d85d03f
Gene <123@gmail.com>
2023-12-04 03:49:16 +0000

    Update JS

--- Script/VVebo-Repair-QX.js
+++ Script/VVebo-Repair-QX.js
@@ -1,19 +1,28 @@
 // 引用地址：https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
+// 更新时间：2023-12-04 11:49:14
+/******************************
+QuantumultX 修复 vvebo 用户主页的显示脚本
+参考：https://raw.githubusercontent.com/suiyuran/stash/main/override/fix-vvebo.stoverride
+*************************
+
+[mitm]
+hostname = api.weibo.cn
+[rewrite_local]
+^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-request-header https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-request-header https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-response-body https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
+*************************
+
+*****************************************/
+
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
-
-let uid;
-
 if (url.includes("users/show")) {
-  if (typeof $persistentStore !== 'undefined') {
-    $persistentStore.write(getUid(url), "uid");
-  } else {
-    $prefs.setValueForKey(getUid(url), "uid");
-  }
-  $done({});
+    $prefs.setValueForKey(getUid(url), "weibouid");
+    $done({});
 } else if (url.includes("statuses/user_timeline")) {
-    try {
+    try{
         let data = JSON.parse($response.body);
         let statuses = data.cards
             .map((card) => (card.card_group ? card.card_group : card))
@@ -28,21 +37,17 @@ if (url.includes("users/show")) {
                 total_number: 100
             })
         });
-    } catch (error) {
-        if (typeof $persistentStore !== 'undefined') {
-            uid = getUid(url) || $persistentStore.read("uid");
-        } else {
-            uid = getUid(url) || $prefs.valueForKey("uid");
-        }
+    } catch {
+        let uid = getUid(url) || $prefs.valueForKey("weibouid");
         url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
         url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
         $done({
             url
         });
     }
+
 } else if (url.includes("profile/statuses/tab")) {
     console.log('ss');
 } else {
     $done({});
 }
-
--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -1,49 +1,25 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2023-12-04 08:45:57
+// 更新时间：2023-12-04 11:49:15
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
-
-let uid;
-
 if (url.includes("users/show")) {
-  if (typeof $persistentStore !== 'undefined') {
-    $persistentStore.write(getUid(url), "weibouid");
-  } else {
-    $prefs.setValueForKey(getUid(url), "weibouid");
-  }
+  $persistentStore.write(getUid(url), "uid");
   $done({});
 } else if (url.includes("statuses/user_timeline")) {
-    try {
-        let data = JSON.parse($response.body);
-        let statuses = data.cards
-            .map((card) => (card.card_group ? card.card_group : card))
-            .flat()
-            .filter((card) => card.card_type === 9)
-            .map((card) => card.mblog);
-        let sinceId = data.cardlistInfo.since_id;
-        $done({
-            body: JSON.stringify({
-                statuses,
-                since_id: sinceId,
-                total_number: 100
-            })
-        });
-    } catch (error) {
-        if (typeof $persistentStore !== 'undefined') {
-            uid = getUid(url) || $persistentStore.read("weibouid");
-        } else {
-            uid = getUid(url) || $prefs.valueForKey("weibouid");
-        }
-        url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
-        url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
-        $done({
-            url
-        });
-    }
+  let uid = getUid(url) || $persistentStore.read("uid");
+  url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
+  url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
+  $done({ url });
 } else if (url.includes("profile/statuses/tab")) {
-    console.log('ss');
+  let data = JSON.parse($response.body);
+  let statuses = data.cards
+    .map((card) => (card.card_group ? card.card_group : card))
+    .flat()
+    .filter((card) => card.card_type === 9)
+    .map((card) => card.mblog);
+  let sinceId = data.cardlistInfo.since_id;
+  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
 } else {
-    $done({});
+  $done({});
 }
-

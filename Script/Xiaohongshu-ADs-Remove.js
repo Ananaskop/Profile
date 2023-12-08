210fff5b9e09ad01ec8c5a637272f469c6f1b671
Gene <123@gmail.com>
2023-12-08 18:12:21 +0000

    Update JS

--- Script/Xiaohongshu-ADs-Remove.js
+++ Script/Xiaohongshu-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/xiaohongshu.js
-// 更新时间：2023-12-08 20:18:03
+// 更新时间：2023-12-09 02:12:20
 // 2023-11-20 21:35
 
 const url = $request.url;

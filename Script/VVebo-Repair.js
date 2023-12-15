a9bbfb841e84ec643f458bc99ae7ca51a7b90eef
Gene <123@gmail.com>
2023-12-15 11:12:11 +0800

    更新文件

--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -4,36 +4,31 @@ let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
 const isQuanX = typeof $task !== "undefined";
-
-let uid; // 将变量声明移到 if 块外部
-
 if (url.includes("users/show")) {
-  uid = getUid(url) || (isQuanX ? $prefs.valueForKey("uid") : $persistentStore.read("uid"));
   if (isQuanX) {
-    $prefs.setValueForKey(uid, "uid");
+    $prefs.setValueForKey(getUid(url), "uid");
   } else {
-    $persistentStore.write(uid, "uid");
+    $persistentStore.write(getUid(url), "uid");
   }
   $done({});
 } else if (url.includes("statuses/user_timeline")) {
-  uid = getUid(url) || (isQuanX ? $prefs.valueForKey("uid") : $persistentStore.read("uid"));
+  if (isQuanX) {
+    let uid = getUid(url) || $prefs.valueForKey("uid");
+  } else {
+    let uid = getUid(url) || $persistentStore.read("uid");
+  }
   url = url.replace("statuses/user_timeline", "profile/statuses/tab").replace("max_id", "since_id");
   url = url + `&containerid=230413${uid}_-_WEIBO_SECOND_PROFILE_WEIBO`;
   $done({ url });
 } else if (url.includes("profile/statuses/tab")) {
-  try {
-    let data = JSON.parse($response.body);
-    let statuses = data.cards
-      .map((card) => (card.card_group ? card.card_group : card))
-      .flat()
-      .filter((card) => card.card_type === 9)
-      .map((card) => card.mblog);
-    let sinceId = data.cardlistInfo.since_id;
-    $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
-  } catch (error) {
-    console.log("解析JSON时发生错误:", error);
-    $done({}); // 优雅地处理错误
-  }
+  let data = JSON.parse($response.body);
+  let statuses = data.cards
+    .map((card) => (card.card_group ? card.card_group : card))
+    .flat()
+    .filter((card) => card.card_type === 9)
+    .map((card) => card.mblog);
+  let sinceId = data.cardlistInfo.since_id;
+  $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
 } else {
   $done({});
 }

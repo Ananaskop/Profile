1f72beb83cc3f191d72219ddffe1376b5e3aaf02
Gene <123@gmail.com>
2023-12-16 02:04:25 +0000

    Update JS

--- Script/NoRedirect.js
+++ Script/NoRedirect.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/noRedirect.js
-// 更新时间：2023-12-16 08:44:35
+// 更新时间：2023-12-16 10:04:25
 /*
 Skip the redirect page.
 
--- Script/VVebo-Repair-QX.js
+++ Script/VVebo-Repair-QX.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
-// 更新时间：2023-12-16 08:44:34
+// 更新时间：2023-12-16 10:04:24
 /******************************
 QuantumultX 修复 vvebo 用户主页的显示脚本
 参考：https://raw.githubusercontent.com/suiyuran/stash/main/override/fix-vvebo.stoverride

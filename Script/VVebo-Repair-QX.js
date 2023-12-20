c464ff3f8c407c12aa930b2819f160c42a15a6f8
Gene <123@gmail.com>
2023-12-20 06:10:41 +0000

    Update JS

--- Script/VVebo-Repair-QX.js
+++ Script/VVebo-Repair-QX.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
-// 更新时间：2023-12-20 08:37:02
+// 更新时间：2023-12-20 14:10:40
 /******************************
 QuantumultX 修复 vvebo 用户主页的显示脚本
 参考：https://raw.githubusercontent.com/suiyuran/stash/main/override/fix-vvebo.stoverride

56aa8c5e5f79101160a56fac331b319f1d4ea797
Gene <123@gmail.com>
2023-12-30 06:11:36 +0000

    Update JS

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2023-12-30 08:43:07
+// 更新时间：2023-12-30 14:11:35
 // 2023-11-28 16:30
 
 const url = $request.url;
--- Script/VVebo-Repair-QX.js
+++ Script/VVebo-Repair-QX.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
-// 更新时间：2023-12-30 08:43:06
+// 更新时间：2023-12-30 14:11:35
 /******************************
 QuantumultX 修复 vvebo 用户主页的显示脚本
 参考：https://raw.githubusercontent.com/suiyuran/stash/main/override/fix-vvebo.stoverride

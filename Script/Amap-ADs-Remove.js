cd7b21aeb99248c65c8815d7afd4737a50157b1e
Gene <123@gmail.com>
2023-12-31 18:10:56 +0000

    Update JS

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2023-12-31 20:16:11
+// 更新时间：2024-01-01 02:10:55
 // 2023-11-28 16:30
 
 const url = $request.url;
--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2023-12-31 20:16:11
+// 更新时间：2024-01-01 02:10:55
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);

ed2d154c046784be4db17d434441bbb7fada98dd
Gene <123@gmail.com>
2023-12-31 12:16:11 +0000

    Update JS

--- Script/VVebo-Repair.js
+++ Script/VVebo-Repair.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2023-12-31 14:11:39
+// 更新时间：2023-12-31 20:16:11
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);

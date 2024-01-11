d67ccb2f1af1bb852bb18af0f1ab48a7c314e32c
Gene <123@gmail.com>
2024-01-11 12:18:10 +0000

    Update JS

--- Script/Amdc-ADs-Remove.js
+++ Script/Amdc-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
-// 更新时间：2024-01-11 14:13:15
+// 更新时间：2024-01-11 20:18:09
 // 2023-08-24 12:45
 
 const url = $request.url;
--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-01-11 14:13:15
+// 更新时间：2024-01-11 20:18:09
 var tlist = {
   1: ["中秋", "2023-09-29"],
   2: ["国庆", "2023-10-01"],

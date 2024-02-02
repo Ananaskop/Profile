ffd3125a22c7eb3518c9d39a46ec30b1bbe3a5bc
Gene <123@gmail.com>
2024-02-02 06:12:01 +0000

    Update JS

--- Script/VVebo-Repair-fans.js
+++ Script/VVebo-Repair-fans.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-fans.js
-// 更新时间：2024-02-02 08:42:56
+// 更新时间：2024-02-02 14:12:00
 let url = $request.url;
 if (url.includes("selffans")) {
   let data = JSON.parse($response.body);

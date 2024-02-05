419b7aa6252564f0e0cbbca135df72e0028cc9e9
Gene <123@gmail.com>
2024-02-05 12:16:50 +0000

    Update JS

--- Script/Caiyun.js
+++ Script/Caiyun.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-// 更新时间：2024-02-05 14:12:29
+// 更新时间：2024-02-05 20:16:48
 // 2023-12-27 16:25
 
 const url = $request.url;
--- Script/VVebo-Repair-timeline.js
+++ Script/VVebo-Repair-timeline.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2024-02-05 14:12:30
+// 更新时间：2024-02-05 20:16:49
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);

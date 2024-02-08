91c943e767a633984484f3e9c056591e3af4f143
Gene <123@gmail.com>
2024-02-08 18:11:30 +0000

    Update JS

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2024-02-08 20:16:39
+// 更新时间：2024-02-09 02:11:29
 // 2024-01-11 17:45
 
 const url = $request.url;
--- Script/VVebo-Repair-fans.js
+++ Script/VVebo-Repair-fans.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-fans.js
-// 更新时间：2024-02-08 20:16:39
+// 更新时间：2024-02-09 02:11:29
 let url = $request.url;
 if (url.includes("selffans")) {
   let data = JSON.parse($response.body);

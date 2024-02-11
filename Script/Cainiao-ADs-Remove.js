5961c6867dedb1113af22d63badbb2c311348b51
Gene <123@gmail.com>
2024-02-11 06:12:17 +0000

    Update JS

--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-02-11 08:46:28
+// 更新时间：2024-02-11 14:12:16
 // 2023-12-19 09:35
 
 const url = $request.url;
--- Script/VVebo-Repair-fans.js
+++ Script/VVebo-Repair-fans.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-fans.js
-// 更新时间：2024-02-11 08:46:27
+// 更新时间：2024-02-11 14:12:16
 let url = $request.url;
 if (url.includes("selffans")) {
   let data = JSON.parse($response.body);

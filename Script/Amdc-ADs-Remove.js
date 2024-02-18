70aea854507214ba455bebcfd426b066d10e854d
Gene <123@gmail.com>
2024-02-18 00:45:51 +0000

    Update JS

--- Script/Amdc-ADs-Remove.js
+++ Script/Amdc-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
-// 更新时间：2024-02-18 02:10:44
+// 更新时间：2024-02-18 08:45:51
 // 2024-02-17 09:45
 
 const url = $request.url;
--- Script/VVebo-Repair-timeline.js
+++ Script/VVebo-Repair-timeline.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2024-02-18 02:10:44
+// 更新时间：2024-02-18 08:45:51
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);

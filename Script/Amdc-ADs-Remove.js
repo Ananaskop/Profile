40fb29300ae435474919c443232280c89d460c5a
Gene <123@gmail.com>
2024-02-19 00:43:32 +0000

    Update JS

--- Script/Amdc-ADs-Remove.js
+++ Script/Amdc-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
-// 更新时间：2024-02-19 02:10:40
+// 更新时间：2024-02-19 08:43:31
 // 2024-02-17 09:45
 
 const url = $request.url;
--- Script/VVebo-Repair-fans.js
+++ Script/VVebo-Repair-fans.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-fans.js
-// 更新时间：2024-02-19 02:10:40
+// 更新时间：2024-02-19 08:43:31
 let url = $request.url;
 if (url.includes("selffans")) {
   let data = JSON.parse($response.body);

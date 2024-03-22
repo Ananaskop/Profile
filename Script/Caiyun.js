895f3ac0fc43a7a625aa60a3a03e7109b5e4ba43
Gene <123@gmail.com>
2024-03-22 00:42:33 +0000

    Update JS

--- Script/Caiyun.js
+++ Script/Caiyun.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-// 更新时间：2024-03-22 02:12:02
+// 更新时间：2024-03-22 08:42:33
 // 2023-12-27 16:25
 
 const url = $request.url;

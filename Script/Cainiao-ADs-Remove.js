e262e0df2e7990d17ba423599eac1d86c93f8ab7
Gene <123@gmail.com>
2024-04-02 00:45:48 +0000

    Update JS

--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-04-02 02:11:52
+// 更新时间：2024-04-02 08:45:48
 // 2024-03-23 19:00
 
 const url = $request.url;

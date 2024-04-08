c55265f61d05ed3f5427cf51c8a230a3254f1039
Gene <123@gmail.com>
2024-04-08 18:13:04 +0000

    Update JS

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2024-04-08 20:17:07
+// 更新时间：2024-04-09 02:13:04
 // 2024-02-20 19:55
 
 const url = $request.url;

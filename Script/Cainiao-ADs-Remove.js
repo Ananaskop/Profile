dae50c410adae49557412309846a7f19faa742ce
Gene <123@gmail.com>
2024-04-08 00:44:22 +0000

    Update JS

--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-04-08 02:11:17
+// 更新时间：2024-04-08 08:44:22
 // 2024-03-23 19:00
 
 const url = $request.url;
--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-04-08 02:11:17
+// 更新时间：2024-04-08 08:44:22
 var tlist = {
   1: ["中秋", "2023-09-29"],
   2: ["国庆", "2023-10-01"],

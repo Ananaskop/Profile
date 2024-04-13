56d1a830a2f82299c646c3b299ded8d7fa6c0052
Gene <123@gmail.com>
2024-04-13 00:36:47 +0000

    Update JS

--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-04-13 02:10:31
+// 更新时间：2024-04-13 08:36:47
 // 2024-03-23 19:00
 
 const url = $request.url;
--- Script/Flush-DNS.js
+++ Script/Flush-DNS.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/Rabbit-Spec/Surge/Master/Module/Panel/Flush-DNS/Moore/Flush-DNS.js
-// 更新时间：2024-04-13 02:10:31
+// 更新时间：2024-04-13 08:36:47
 /*
  * 由@zZPiglet编写
  * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js

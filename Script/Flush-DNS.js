936fc4ba932803c41fac61c91423936b156ce2a8
Gene <123@gmail.com>
2024-05-17 12:17:58 +0000

    更新相关文件

--- Script/Flush-DNS.js
+++ Script/Flush-DNS.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/Rabbit-Spec/Surge/Master/Module/Panel/Flush-DNS/Moore/Flush-DNS.js
-// 更新时间：2024-05-17 14:13:53
+// 更新时间：2024-05-17 20:17:56
 /*
  * 由@zZPiglet编写
  * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js

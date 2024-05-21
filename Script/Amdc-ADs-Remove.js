37eac8f3c8eeb360125e147198b7ed5c2c67f318
Gene <123@gmail.com>
2024-05-21 06:14:41 +0000

    更新相关文件

--- Script/Amdc-ADs-Remove.js
+++ Script/Amdc-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
-// 更新时间：2024-05-21 08:46:43
+// 更新时间：2024-05-21 14:14:40
 // 2024-03-15 14:45
 
 const url = $request.url;

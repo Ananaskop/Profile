4f2542d19ebf00dfa0bc2c361afb20a189b9ef61
Gene <123@gmail.com>
2024-05-22 00:47:02 +0000

    更新相关文件

--- Script/Caiyun.js
+++ Script/Caiyun.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-// 更新时间：2024-05-22 02:12:59
+// 更新时间：2024-05-22 08:47:01
 // 2023-12-27 16:25
 
 const url = $request.url;
--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-05-22 02:12:59
+// 更新时间：2024-05-22 08:47:01
 var tlist = {
   1: ["端午", "2024-06-10"],
   2: ["中秋", "2024-09-17"],

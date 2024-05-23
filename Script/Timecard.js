e1556bc9a711d8a93f0e9f709a9203487aa52e64
Gene <123@gmail.com>
2024-05-23 18:13:14 +0000

    更新相关文件

--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-05-23 20:18:37
+// 更新时间：2024-05-24 02:13:13
 var tlist = {
   1: ["端午", "2024-06-10"],
   2: ["中秋", "2024-09-17"],

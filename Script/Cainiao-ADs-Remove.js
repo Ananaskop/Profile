540d8a324e1598e2fb2e13f7a8db775f184c00f7
Gene <123@gmail.com>
2024-06-13 18:14:23 +0000

    更新相关文件

--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-06-13 20:19:41
+// 更新时间：2024-06-14 02:14:21
 // 2024-06-08 09:40
 
 const url = $request.url;
--- Script/Caiyun.js
+++ Script/Caiyun.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-// 更新时间：2024-06-13 20:19:41
+// 更新时间：2024-06-14 02:14:22
 // 2023-12-27 16:25
 
 const url = $request.url;

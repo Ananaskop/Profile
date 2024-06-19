5478d4aa4bb6090088f50423a6f9eedd1046c74f
Gene <123@gmail.com>
2024-06-19 18:12:30 +0000

    更新相关文件

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2024-06-19 20:19:42
+// 更新时间：2024-06-20 02:12:29
 // 2024-06-11 20:25
 
 const url = $request.url;
--- Script/Flush-DNS.js
+++ Script/Flush-DNS.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/Rabbit-Spec/Surge/Master/Module/Panel/Flush-DNS/Moore/Flush-DNS.js
-// 更新时间：2024-06-19 20:19:42
+// 更新时间：2024-06-20 02:12:29
 /*
  * 由@zZPiglet编写
  * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js

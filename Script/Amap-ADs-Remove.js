0c09c08c1936ac108e983f87b0cab1344c166d4d
Gene <123@gmail.com>
2024-06-25 06:14:52 +0000

    更新相关文件

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2024-06-25 08:49:49
+// 更新时间：2024-06-25 14:14:51
 // 2024-06-11 20:25
 
 const url = $request.url;
--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-06-25 08:49:49
+// 更新时间：2024-06-25 14:14:51
 // 2024-06-08 09:40
 
 const url = $request.url;

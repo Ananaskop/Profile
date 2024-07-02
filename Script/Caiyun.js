126791e61869d08a8f9716e16338bd49d341c577
Gene <123@gmail.com>
2024-07-02 00:50:12 +0000

    更新相关文件

--- Script/Caiyun.js
+++ Script/Caiyun.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/break/caiyun.js
-// 更新时间：2024-07-02 02:14:09
+// 更新时间：2024-07-02 08:50:11
 // 2023-12-27 16:25
 
 const url = $request.url;
--- Script/Flush-DNS.js
+++ Script/Flush-DNS.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/Rabbit-Spec/Surge/Master/Module/Panel/Flush-DNS/Moore/Flush-DNS.js
-// 更新时间：2024-07-02 02:14:09
+// 更新时间：2024-07-02 08:50:12
 /*
  * 由@zZPiglet编写
  * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js

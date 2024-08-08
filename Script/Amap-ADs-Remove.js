a1010d598af0995f80eebc701c8e347043ba9575
Gene <123@gmail.com>
2024-08-08 18:15:16 +0000

    更新相关文件

--- Script/Amap-ADs-Remove.js
+++ Script/Amap-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
-// 更新时间：2024-08-08 20:20:22
+// 更新时间：2024-08-09 02:15:16
 // 2024-08-07 19:25
 
 const url = $request.url;
--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-08-08 20:20:22
+// 更新时间：2024-08-09 02:15:16
 var tlist = {
   1: ["端午", "2024-06-10"],
   2: ["中秋", "2024-09-17"],

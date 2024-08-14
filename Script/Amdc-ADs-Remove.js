1d62f7e536cb967752f81424dbf9516dd8e46605
Gene <123@gmail.com>
2024-08-14 00:53:01 +0000

    更新相关文件

--- Script/Amdc-ADs-Remove.js
+++ Script/Amdc-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
-// 更新时间：2024-08-14 02:14:53
+// 更新时间：2024-08-14 08:53:00
 // 2024-08-11 13:40
 
 const url = $request.url;
--- Script/Cainiao-ADs-Remove.js
+++ Script/Cainiao-ADs-Remove.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
-// 更新时间：2024-08-14 02:14:53
+// 更新时间：2024-08-14 08:53:00
 // 2024-06-08 09:40
 
 const url = $request.url;

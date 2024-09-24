1561986507c5c50a8e63fd8f94daabe18fb5d4cd
Ananaskop <123@gmail.com>
2024-09-24 00:54:39 +0000

    JS_Update

--- Script/Pinduoduo.js
+++ Script/Pinduoduo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Pinduoduo.js
-// 更新时间：2024-09-24 02:14:44
+// 更新时间：2024-09-24 08:54:37
 // 2024.08.13
 const url = $request.url;
 let obj = JSON.parse($response.body);
--- Script/VVebo_repair.js
+++ Script/VVebo_repair.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
-// 更新时间：2024-09-24 02:14:44
+// 更新时间：2024-09-24 08:54:37
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);

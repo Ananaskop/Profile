9c070e87ebc44c23be687ae07a69e8e5003dc418
Ananaskop <123@gmail.com>
2024-09-30 00:56:20 +0000

    JS_Update

--- Script/Pinduoduo.js
+++ Script/Pinduoduo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Pinduoduo.js
-// 更新时间：2024-09-30 02:13:48
+// 更新时间：2024-09-30 08:56:19
 // 2024.08.13
 const url = $request.url;
 let obj = JSON.parse($response.body);
--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-09-30 02:13:48
+// 更新时间：2024-09-30 08:56:18
 var tlist = {
   1: ["国庆", "2024-10-01"],
   2: ["元旦", "2025-01-01"],

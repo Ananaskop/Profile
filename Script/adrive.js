edb803f24255923a47af92d4f42850982f5be8a3
Ananaskop <123@gmail.com>
2024-09-30 06:17:02 +0000

    JS_Update

--- Script/adrive.js
+++ Script/adrive.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/adrive.js
-// 更新时间：2024-09-30 08:56:18
+// 更新时间：2024-09-30 14:16:59
 // 2024-07-15 21:30
 
 const url = $request.url;
--- Script/baiduNoRedirect.js
+++ Script/baiduNoRedirect.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/baidu/baiduNoRedirect.js
-// 更新时间：2024-09-30 08:56:19
+// 更新时间：2024-09-30 14:17:01
 /*
 引用地址 https://github.com/app2smile/rules/blob/master/js/baidu-no-redirect.js
 

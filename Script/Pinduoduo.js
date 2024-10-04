1f0631b3b3bf603eb313dc368d21e23e8cbf9abe
Ananaskop <123@gmail.com>
2024-10-04 00:54:16 +0000

    JS_Update

--- Script/Pinduoduo.js
+++ Script/Pinduoduo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Pinduoduo.js
-// 更新时间：2024-10-04 02:15:08
+// 更新时间：2024-10-04 08:54:14
 // 2024.08.13
 const url = $request.url;
 let obj = JSON.parse($response.body);
--- Script/baiduNoRedirect.js
+++ Script/baiduNoRedirect.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/baidu/baiduNoRedirect.js
-// 更新时间：2024-10-04 02:15:09
+// 更新时间：2024-10-04 08:54:15
 /*
 引用地址 https://github.com/app2smile/rules/blob/master/js/baidu-no-redirect.js
 

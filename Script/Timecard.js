a2ed807532bfe8fd9b28eb7a6dfa6122407afdf4
Ananaskop <123@gmail.com>
2024-10-05 06:14:29 +0000

    JS_Update

--- Script/Timecard.js
+++ Script/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-10-05 08:53:43
+// 更新时间：2024-10-05 14:14:26
 var tlist = {
   1: ["国庆", "2024-10-01"],
   2: ["元旦", "2025-01-01"],
--- Script/xiaohongshu.js
+++ Script/xiaohongshu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/xiaohongshu.js
-// 更新时间：2024-10-05 08:53:44
+// 更新时间：2024-10-05 14:14:27
 // 2023-12-20 19:00
 
 const url = $request.url;

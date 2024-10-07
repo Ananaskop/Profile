3773081ee0ea0f01afb55228fc7e8505daead76f
Ananaskop <123@gmail.com>
2024-10-07 00:56:09 +0000

    JS_Update

--- Script/12306.js
+++ Script/12306.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/12306.js
-// 更新时间：2024-10-07 02:13:32
+// 更新时间：2024-10-07 08:56:09
 // 2024-02-19 10:45
 
 let body = "";
--- Script/adrive.js
+++ Script/adrive.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/adrive.js
-// 更新时间：2024-10-07 02:13:29
+// 更新时间：2024-10-07 08:56:07
 // 2024-07-15 21:30
 
 const url = $request.url;

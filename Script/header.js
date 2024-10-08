02a649d77ea4f73a2e980acd592428cedabcc6e7
Ananaskop <123@gmail.com>
2024-10-08 06:16:30 +0000

    JS_Update

--- Script/header.js
+++ Script/header.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/header.js
-// 更新时间：2024-10-08 08:54:09
+// 更新时间：2024-10-08 14:16:29
 // 2024-08-11 13:40
 
 const url = $request.url;
--- Script/xianyu.js
+++ Script/xianyu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/luestr/ProxyResource/main/Resource/Script/FleaMarket/FleaMarket_remove_ads.js
-// 更新时间：2024-10-08 08:54:09
+// 更新时间：2024-10-08 14:16:28
 // 2024-08-11 13:53:43
 const url = $request.url;
 let obj = JSON.parse($response.body);

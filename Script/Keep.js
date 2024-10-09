13e4dae27e9700765eebee24f5dfce8a09a12799
Ananaskop <123@gmail.com>
2024-10-09 12:21:07 +0000

    JS_Update

--- Script/Keep.js
+++ Script/Keep.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Keep.js
-// 更新时间：2024-10-09 14:19:13
+// 更新时间：2024-10-09 20:21:07
 //   2024-05-24 09.38
 const url = $request.url;
 let obj = JSON.parse($response.body);
--- Script/myBlockAds.js
+++ Script/myBlockAds.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/myBlockAds.js
-// 更新时间：2024-10-09 14:19:13
+// 更新时间：2024-10-09 20:21:05
 // 2024-09-22 12:25
 
 const url = $request.url;

5b32fa6b63c1783853aad77ddc87236a8e988303
Ananaskop <123@gmail.com>
2024-10-09 06:19:14 +0000

    JS_Update

--- Script/baiduNoRedirect.js
+++ Script/baiduNoRedirect.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/baidu/baiduNoRedirect.js
-// 更新时间：2024-10-09 14:16:45
+// 更新时间：2024-10-09 14:19:13
 /*
 引用地址 https://github.com/app2smile/rules/blob/master/js/baidu-no-redirect.js
 

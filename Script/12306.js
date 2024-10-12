ec16de8487b67031a3f94cd64418926aaf08ede6
Ananaskop <123@gmail.com>
2024-10-12 00:04:58 +0000

    JS_Update

--- Script/12306.js
+++ Script/12306.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/12306.js
-// 更新时间：2024-10-12 02:14:44
+// 更新时间：2024-10-12 08:04:58
 // 2024-02-19 10:45
 
 let body = "";

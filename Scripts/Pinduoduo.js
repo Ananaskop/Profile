3cd6c32df2febcae29d016161bf3e17ef332571a
Ananaskop <123@gmail.com>
2024-10-12 02:41:09 +0000

    JS_Update

--- Scripts/Pinduoduo.js
+++ Scripts/Pinduoduo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Pinduoduo.js
-// 更新时间：2024-10-12 08:53:04
+// 更新时间：2024-10-12 10:41:08
 // 2024.08.13
 const url = $request.url;
 let obj = JSON.parse($response.body);

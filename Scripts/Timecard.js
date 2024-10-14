fc8aba7395d172c8e6b58c48b66667273a692c6f
Ananaskop <123@gmail.com>
2024-10-14 18:15:07 +0000

    JS_Update

--- Scripts/Timecard.js
+++ Scripts/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-10-14 20:21:51
+// 更新时间：2024-10-15 02:15:06
 var tlist = {
   1: ["国庆", "2024-10-01"],
   2: ["元旦", "2025-01-01"],
--- Scripts/baiduNoRedirect.js
+++ Scripts/baiduNoRedirect.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/baidu/baiduNoRedirect.js
-// 更新时间：2024-10-14 20:21:51
+// 更新时间：2024-10-15 02:15:06
 /*
 引用地址 https://github.com/app2smile/rules/blob/master/js/baidu-no-redirect.js
 

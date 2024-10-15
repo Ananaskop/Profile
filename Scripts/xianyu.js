b57057d2cdf10e0b94acda0b03e1329d6f8e2deb
Ananaskop <123@gmail.com>
2024-10-15 00:55:11 +0000

    JS_Update

--- Scripts/xianyu.js
+++ Scripts/xianyu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xianyu.js
-// 更新时间：2024-10-15 02:15:04
+// 更新时间：2024-10-15 08:55:09
 // 2024-10-12 15:05
 
 const url = $request.url;

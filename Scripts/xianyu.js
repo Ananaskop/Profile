ee0412cfa438e609a3e531aa0f306af639620221
Ananaskop <123@gmail.com>
2024-10-18 05:21:51 +0000

    JS_Update

--- Scripts/xianyu.js
+++ Scripts/xianyu.js
@@ -1,6 +1,6 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xianyu.js
-// 更新时间：2024-10-18 09:54:51
-// 2024-10-12 15:05
+// 更新时间：2024-10-18 13:21:49
+// 2024-10-18 12:50
 
 const url = $request.url;
 if (!$response) $done({});
@@ -18,6 +18,13 @@ if (url.includes("/mtop.idle.user.page.my.adapter/")) {
       } else if (items?.template?.name === "my_fy25_slider") {
         // 滚动小提示
         continue;
+      } else if (items?.template?.name === "my_fy25_tools") {
+        // 工具箱 14借钱
+        if (items?.item?.tool?.exContent?.tools?.length > 0) {
+          items.item.tool.exContent.tools = items.item.tool.exContent.tools.map((i) =>
+            i.filter((ii) => ![14]?.includes(ii?.exContent?.toolId))
+          );
+        }
       } else if (items?.template?.name === "xianyu_home_fish_my_banner_card_2023") {
         continue;
       }
@@ -44,11 +51,12 @@ if (url.includes("/mtop.idle.user.page.my.adapter/")) {
     );
   }
   if (obj?.data?.widgetReturnDO?.widgets?.length > 0) {
-    let widget = obj?.data?.widgetReturnDO?.widgets[0];
+    const sortLists = ["分类", "手机数码", "上门回收", "闲鱼鱼市", "闲鱼鉴别"];
+    const widget = obj?.data?.widgetReturnDO?.widgets[0];
     if (widget?.widgetDO?.channelDOList?.length > 0) {
-      widget.widgetDO.channelDOList = widget.widgetDO.channelDOList.filter((i) =>
-        ["手机数码", "分类", "上门回收", "闲鱼鱼市", "闲鱼鉴别", ""]?.includes(i?.title)
-      );
+      widget.widgetDO.channelDOList = widget.widgetDO.channelDOList
+        .filter((i) => sortLists?.includes(i?.title))
+        .sort((a, b) => sortLists.indexOf(a?.title) - sortLists.indexOf(b?.title));
     }
   }
 } else if (url.includes("/mtop.taobao.idlehome.home.circle.list/")) {
@@ -72,7 +80,7 @@ if (url.includes("/mtop.idle.user.page.my.adapter/")) {
           i?.data?.template?.name === "idlefish_seafood_vote" || // 搜索结果 投票
           i?.data?.template?.name === "idlefish_search_card_category_select" || // 大家都在搜
           i?.data?.item?.main?.exContent?.isAliMaMaAD === "true" || // 广告1
-          i?.data?.item?.main?.exContent?.isAliMaMaAD === true // 广告2
+          i?.data?.item?.main?.exContent?.isAliMaMaAD === true
         )
     );
   }
--- Scripts/xiaohongshu.js
+++ Scripts/xiaohongshu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/xiaohongshu.js
-// 更新时间：2024-10-18 09:54:49
+// 更新时间：2024-10-18 13:21:48
 // 2024-10-15 10:45
 
 const url = $request.url;

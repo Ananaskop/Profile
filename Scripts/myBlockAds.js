abfe7a02d0da1ed77390c52f185083d7c823178f
Ananaskop <ananaskop@icloud.com>
2024-10-21 07:12:48 +0800

    更新 myBlockAds.js

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/myBlockAds.js
+// 引用：https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/myBlockAds.js
 // 更新时间：2024-10-20 20:20:52
 // 2024-10-15 10:05
 

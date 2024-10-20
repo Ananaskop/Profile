29e2edd4ad891ee8d01a2e8f09181697b872baa7
Ananaskop <ananaskop@icloud.com>
2024-10-21 07:13:10 +0800

    更新 xianyu.js

--- Scripts/xianyu.js
+++ Scripts/xianyu.js
@@ -1,4 +1,4 @@
-// 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xianyu.js
+// 引用：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xianyu.js
 // 更新时间：2024-10-20 20:20:53
 // 2024-10-18 12:50
 

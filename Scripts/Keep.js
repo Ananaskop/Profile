0aa8ac5fb772308c94602fb52e11613f71821070
Ananaskop <ananaskop@icloud.com>
2024-10-22 16:14:38 +0800

    更新 Keep.js

--- Scripts/Keep.js
+++ Scripts/Keep.js
@@ -1,5 +1,16 @@
 //   2024-05-24 09.38
 const blackList = ["home", "entry", "mall"];
+let obj = JSON.parse($response.body);
+
+obj.data.generalConfigs["AD.link.download.source"] = false;
+// 设置默认开启页面
+obj.data.bottomBarControl.defaultTab = "new_sports";
+
+obj.data.bottomBarControl.tabs = obj.data.bottomBarControl.tabs.filter(
+  (tab) => !blackList.includes(tab.tabType)
+);
+
+$done({ body: JSON.stringify(obj) });
 const url = $request.url;
 let obj = JSON.parse($response.body);
 

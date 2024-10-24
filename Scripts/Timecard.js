75d6a733cea44314d6e9c28024e3839c340a2911
Ananaskop <123@gmail.com>
2024-10-24 06:16:46 +0000

    JS_Update

--- Scripts/Timecard.js
+++ Scripts/Timecard.js
@@ -1,5 +1,5 @@
 // 引用地址：https://raw.githubusercontent.com/smartmimi/conf/master/surge/timecard.js
-// 更新时间：2024-10-24 08:54:24
+// 更新时间：2024-10-24 14:16:46
 var tlist = {
   1: ["国庆", "2024-10-01"],
   2: ["元旦", "2025-01-01"],
